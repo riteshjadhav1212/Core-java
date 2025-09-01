34aeef958ddc5f81cdc054d67dfa598520ed3d4b
DELL <DELL@Ritx>
2025-09-01 17:10:26 +0530

    added one more line

--- Practicals/src/com/vspl/demo/Employee.java
+++ Practicals/src/com/vspl/demo/Employee.java
@@ -5,6 +5,8 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Something");
 		System.out.println("new line added");
+		System.out.println("new one more line");
+
 
 	}
 
